bd52ffd6a51ed63980f6694ffbbdf2bb227bdf05
centos <moritalous@gmail.com>
2018-01-14 17:56:46 +0900

    Add setup script

---
+++ create_deploypackage.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+# zip
+zip -r9 DeployPackage.zip *
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+# Get ffmpeg
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
+tar xvf ffmpeg-release-64bit-static.tar.xz
+mv ffmpeg-*-64bit-static/ffmpeg ./
+
+rm -rf ffmpeg-*-64bit-static
+rm -rf ffmpeg-release-64bit-static.tar.xz
+
+# pip install
+pip install -r requirements.txt -t ./
